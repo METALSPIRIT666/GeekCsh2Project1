469d958a003941ce584e6907f71d70a5b15d310a
METALSPIRIT666 <anabatov@mail.ru>
2018-06-26 22:31:46 +0300

    Добавлены классы астероида и снаряда.

---
+++ GeekCsh2Project1/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace GeekCsh2Project1
+{
+    class Bullet : BaseObject
+    {
+        public Bullet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        public override void Draw()
+        {
+            Game.buffer.Graphics.DrawRectangle(Pens.OrangeRed, pos.X,                pos.Y, size.Width, size.Height);
+        }
+
+        public override void Update()
+        {
+            pos.X += 3;
+        }
+
+        
+    }
+}
